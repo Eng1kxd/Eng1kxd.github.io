7513fda4f3979f3ed3889959239003e80a21b621
Eng1kxd <168446261+Eng1kxd@users.noreply.github.com>
2025-01-31 09:50:25 +0400

    Add files via upload

---
+++ src/UI/ButtonOrder.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ButtonOrder = () => {
+  return (
+    <button className='button fnsz16 bold ButtonOrder'
+    type="submit">
+      Заказать
+    </button>
+  )
+}
+
+export default ButtonOrder
